edf5c7dac63d7417c5d5f92cbe9db983e0efa8a8
Carlos <cfuentesbarraza30@gmail.com>
2020-09-15 23:15:09 -0600

    bichayal
    lista de eventos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {TicketsdeusuarioComponent} from './features/ticketsdeusuario/ticketsdeusuario.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path: 'ticketsusuarios', component: TicketsdeusuarioComponent}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TicketsdeusuarioComponent } from 'src/app/features/ticketsdeusuario/ticketsdeusuario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TicketsdeusuarioComponent
   ],
   imports: [
     BrowserModule,
