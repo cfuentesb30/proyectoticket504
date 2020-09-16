36ccd4c4d8baa00da76a86732c40f96baa52dc54
alekseymartinov0501 <67990606+alekseymartinov0501@users.noreply.github.com>
2020-09-15 23:25:27 -0600

    cosa
    cosa

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactanosComponent} from "./features/contactanos/contactanos.component";
+import { RecuperarClaveComponent} from "./features/recuperar-clave/recuperar-clave.component";
+import { SesionUsuarioComponent} from "./features/sesion-usuario/sesion-usuario.component";
 
-const routes: Routes = [];
+ 
+
+const routes: Routes = [
+  {path:'contactanos',component:ContactanosComponent},{path: 'recuperarclave',component:RecuperarClaveComponent},{path: 'sesionusuario',component:SesionUsuarioComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SesionUsuarioComponent } from './features/sesion-usuario/sesion-usuario.component';
+import { ContactanosComponent } from './features/contactanos/contactanos.component';
+import { RecuperarClaveComponent } from './features/recuperar-clave/recuperar-clave.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SesionUsuarioComponent,
+    ContactanosComponent,
+    RecuperarClaveComponent
   ],
   imports: [
     BrowserModule,
